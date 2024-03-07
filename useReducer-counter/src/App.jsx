cc4e284eef8694b966e2d630d030dff9fa31530d
ayan878 <ayanraza197@gmail.com>
2024-03-07 19:50:45 +0530

    useReducer-counter

--- useReducer-counter/src/App.jsx
+++ useReducer-counter/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { initialState, reducer } from "./countReducer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const increment = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
+  const reset = () => {
+    dispatch({ type: "reset" });
+  };
 
   return (
     <>
@@ -16,20 +28,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Reducer-Counter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={decrement}>-</button>
+        <h2 onClick={reset}>{state.count}</h2>
+        <button onClick={increment}>+</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
