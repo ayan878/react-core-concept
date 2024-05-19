8e056554c3c82c7033f4782578b82c4cb0dc55d5
ayan878 <ayanraza197@gmail.com>
2024-05-19 23:40:41 +0530

    auth

--- Redux-Shopping-Cart-App/src/index.js
+++ Redux-Shopping-Cart-App/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "@reduxjs/toolkit";
+import store from "./store/index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
