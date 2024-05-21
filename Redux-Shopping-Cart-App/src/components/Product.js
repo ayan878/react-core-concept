291c9bbe74b209a1053688d27bbb8c138535080f
ayan878 <ayanraza197@gmail.com>
2024-05-22 01:54:02 +0530

    product

--- Redux-Shopping-Cart-App/src/components/Product.js
+++ Redux-Shopping-Cart-App/src/components/Product.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 import "./Product.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../store/cartSlice";
 const Product = ({ name, id, imgURL, price }) => {
+  const cartitem = useSelector((state) => state.cart.itemList);
+  console.log(cartitem);
+  const dispatch = useDispatch();
   return (
     <div className="card">
       <img src={imgURL} alt={name} />
       <h2>{name}</h2>
       <p>$ {price}</p>
-      <button>Add to cart</button>
+      <button onClick={() => dispatch(addToCart({name, id, price}))}>
+        Add to cart
+      </button>
     </div>
   );
 };
