c6a515fa788c9717c91578ba7ed66b50df65ae7f
ayan878 <ayanraza197@gmail.com>
2024-07-02 16:32:42 +0530

    contextApi

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
-import MyComponent from "./MyComponent";
-import { myContext } from "./MyContext";
+import React, { createContext, useContext, useState } from "react";
+
+// Create context outside the component
+const MyContext = createContext(0);
+
+const MyComponent = () => {
+  // Use context inside component
+  const { count, setCount } = useContext(MyContext);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>+</button>
+    </div>
+  );
+};
 
 function App() {
- 
+  // Manage state in App component
   const [count, setCount] = useState(0);
+
   return (
-    <myContext.Provider value={{count, setCount}}>
-      <MyComponent />
-    </myContext.Provider>
+    <MyContext.Provider value={{ count, setCount }}>
+      <div>
+        <MyComponent />
+      </div>
+    </MyContext.Provider>
   );
 }
 
